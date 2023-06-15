b092fad2f863aac4593d7849b665958c19e43f66
DamianOribeVlim <damianortiz@vlim.com.mx>
2023-06-15 13:01:02 -0600

    Ruta actualizada

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -45,7 +45,7 @@ const selectTones = (tonesSelected) => {
 };
 
 const translate = () => {
-  fetch("http://localhost:3001/translate", {
+  fetch("https://main.d26q3251ptvemd.amplifyapp.com/translate", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
